6710d2c6f0ee03000ced4ee9b900d43aa5c9b0ec
kaiserl4961 <ltklionel@gmail.com>
2022-12-07 20:58:49 -0600

    updated stuff

--- vexcodepro/tools/chocolateyinstall.ps1
+++ vexcodepro/tools/chocolateyinstall.ps1
@@ -1,26 +1,31 @@
 ﻿$ErrorActionPreference = 'Stop'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://content.vexrobotics.com/vexcode/v5code/VEXcodeProV5_2_0_5.exe'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://content.vexrobotics.com/vexcode/v5code/VEXcodeProV5_2_0_5.exe'
 
 $cert = Get-ChildItem Cert:\CurrentUser\TrustedPublisher -Recurse | Where-Object { $_.Thumbprint -eq '548132fd02be43149355d0596c256c0fd4c5c578' }
 if (!$cert) {
-    $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-    Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$toolsPath\VEXrobotics.cer'"
+  $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+  Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$toolsPath\VEXrobotics.cer'"
+}
+$cert1 = Get-ChildItem Cert:\CurrentUser\TrustedPublisher -Recurse | Where-Object { $_.Thumbprint -eq '24369f7f00a9d19d11bbf460215027cda25d99a8' }
+if (!$cert1) {
+  $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+  Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$toolsPath\Robomatter.cer'"
 }
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
 
-  softwareName  = 'VEXcode Pro*'
+  softwareName   = 'VEXcode Pro*'
 
-  checksum      = 'F56794057E91A101969D3185190730BF77DB0C13B85BE0F44040DF0D12F3A3C2'
-  checksumType  = 'sha256'
+  checksum       = 'F56794057E91A101969D3185190730BF77DB0C13B85BE0F44040DF0D12F3A3C2'
+  checksumType   = 'sha256'
 
-  silentArgs    = "/SD /v/qn"
-  validExitCodes= @(0, 3010, 1641)
+  silentArgs     = "/SD /v/qn"
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
