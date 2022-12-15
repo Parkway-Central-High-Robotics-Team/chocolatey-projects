e5adc555b2d6b96d13b114d23bd6343f68f9998c
kaiserl4961 <ltklionel@gmail.com>
2022-12-15 00:00:01 -0600

    updated vexcode

--- vexcode/tools/chocolateyinstall.ps1
+++ vexcode/tools/chocolateyinstall.ps1
@@ -1,25 +1,31 @@
-
 $ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://content.vexrobotics.com/vexcode/v5blocks/VEXcodeV5-20220829.exe'
-$url64 = 'https://content.vexrobotics.com/vexcode/v5blocks/VEXcodeV5-20220829.exe'
+
+$cert = Get-ChildItem Cert:\CurrentUser\TrustedPublisher -Recurse | Where-Object { $_.Thumbprint -eq '548132fd02be43149355d0596c256c0fd4c5c578' }
+if (!$cert) {
+  $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+  Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$toolsPath\VEXrobotics.cer'"
+}
+$cert1 = Get-ChildItem Cert:\CurrentUser\TrustedPublisher -Recurse | Where-Object { $_.Thumbprint -eq '24369f7f00a9d19d11bbf460215027cda25d99a8' }
+if (!$cert1) {
+  $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+  Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$toolsPath\Robomatter.cer'"
+}
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
   url            = $url
-  url64bit       = $url64
 
   softwareName   = 'VEXcode*'
 
-  checksum       = ''
+  checksum       = '604CD7FF881084C36D9F95537FA2AF8DA52296AE57A9064E26C3E1900967CE59'
   checksumType   = 'sha256'
-  checksum64     = ''
-  checksumType64 = 'sha256'
 
+  silentArgs     = '/SD /v"/qn"'
   validExitCodes = @(0, 3010, 1641)
-  silentArgs   = '/SD /v"/qn"'
 }
 
 Install-ChocolateyPackage @packageArgs
